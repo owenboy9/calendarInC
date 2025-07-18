eb89417ae1ed2a22ebe0e3022f307d828e4fff94
owenboy9 <owenkraus9@gmail.com>
2025-07-18 12:49:47 +0200

    prevent user from entering anything but ints in promptInt

--- src/inpututils.c
+++ src/inpututils.c
@@ -20,12 +20,26 @@ void promptString(const char *label, char *field, size_t maxLen, int showCurrent
 void promptInt(const char *label, int *field, int showCurrent) {
     char buf[100];
 
-    if (showCurrent) printf("%s (currently %d): ", label, *field);
-    else printf("%s: ", label);
-    
-    if (fgets(buf, sizeof(buf), stdin)) {
-        if (buf[0] != '\n') {
-            sscanf(buf, "%d", field);
+    while (1) {
+        if (showCurrent) 
+            printf("%s (currently %d): ", label, *field);
+        else 
+            printf("%s: ", label);
+
+        if (!fgets(buf, sizeof(buf), stdin)) {
+            printf("input error. \n");
+            continue;
         }
+
+        if (buf[0] == '\n')
+            break;
+
+        buf[strcspn(buf, "\n")] = '\0';   // remove newline
+
+        char temp;
+        if (sscanf(buf, "%d %c", field, &temp) == 1)
+            break;
+        else
+            printf("invalid input. type in a number or press enter to move on.");
     }
 }
