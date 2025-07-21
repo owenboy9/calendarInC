3b7afae411ee575c21673d36be2b7da72150d383
owenboy9 <owenkraus9@gmail.com>
2025-07-21 12:59:23 +0200

    small changes in menu, start ui.c

--- src/menu.c
+++ src/menu.c
@@ -2,21 +2,28 @@
 #include <termios.h>
 #include <string.h>
 #include <unistd.h>
-
-#define MAX_OPTIONS 10
+#include <ctype.h>
 
 void disableRawMode(struct termios *orig_termios) {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, orig_termios);
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, orig_termios);  // flush and restore original terminal settings (constants used by system calls to manipulate terminal i/o settings through termios api)
 }
+
+/*
+TCSANOW — apply immediately.
+
+TCSADRAIN — wait for output to drain, then apply.
+
+TCSAFLUSH — wait for output to drain, and discard any unread input.
+*/
 
 void enableRawMode(struct termios *orig_termios) {
     struct termios raw;
 
-    tcgetatts(STDIN_FILENO, orig_termios);
-    raw = *orig_termios;
+    tcgetattr(STDIN_FILENO, orig_termios);  // store current terminal attributes
+    raw = *orig_termios;  // make a copy to modify
 
-    raw.c_lflag &= ~(ECHO | ICANON);
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    raw.c_lflag &= ~(ECHO | ICANON);  // disable canonical mode (user must press enter to submit input) and echo (typed chars won't be displayed))
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);  // apply the modified settings
 }
 
 int arrowMenu(const char *title, const char *options[], int numOptions) {
@@ -24,21 +31,21 @@ int arrowMenu(const char *title, const char *options[], int numOptions) {
     enableRawMode(&orig_termios);
 
     int selected = 0;
-    int ch;
+    char ch;
 
     while (1) {
-        printf("\033[H\033[J"); // clear screen
+        printf("\033[2J\033[H"); // clear screen: \033[2J = ANSI escape code to clear the screen; \033[H Moves cursor to the home position (top-left)
         printf("-----> %s <-----\n", title);
         for (int i = 0; i < numOptions; i++) {
             if (i == selected) {
-                printf(" → \033[1m%s\033[0m\n", options[i]);
+                printf(" → \033[1m%s\033[0m\n", options[i]);  // highlight
             } else {
                 printf("   %s\n", options[i]);
             }
         }
 
         ch = getchar();
-        if (ch == 'q') {
+        if (tolower(ch) == 'q') {
             break; // exit on 'q'
         } else if (ch == '\033') { // escape sequence
             getchar(); // skip the '[' character
